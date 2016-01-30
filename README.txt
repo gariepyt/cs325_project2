f0eb713f5973efe47f6a8db3547ce860c4844e11
gariepyt <tom@tomgariepy.com>
2016-01-30 11:27:30 -0800

    Added additional files
    Included my copy of the change greedy function. My code for opening and
    reading the files works and the required arguments for running the file
    should match the requirements given to us in the assignment.

--- README.txt
+++ README.txt
@@ -3,4 +3,4 @@ Project 2 Group 24
 Tom Gariepy, Jeffrey Schachtsick, Benjamin Frishman
 
 
-Type "python cs325w16_g24_project2.py"
+Type "python cs325w16_g24_project2.py [filename without .txt]"
---
+++ tom_changeGreedy.py
@@ -0,0 +1,73 @@
+# Greedy method for finding umber of arrays in 
+# Written by: Tom Gariepy
+
+import sys
+import os
+
+def chGreedy(array, total):
+	i = len(array) - 1
+	curTotal = total
+	countArr =  [0] * len(array)
+
+	while (curTotal > 0):
+		if (array[i] <= curTotal):
+			curTotal = curTotal - array[i]
+			countArr[i] = countArr[i] + 1
+		else:
+			i = i - 1
+
+	totCount = 0
+
+	for x in range(0, len(countArr)):
+		totCount = totCount + countArr[x]
+
+	return countArr, totCount
+
+
+def main():
+
+	if (len(sys.argv) == 2):
+		fName = sys.argv[1]
+		inName = fName + ".txt"
+		outName = fName + "change.txt"
+
+		fileCheck = os.path.isfile(inName)
+		if (fileCheck == False):
+			print("ERROR: " + inName + " not found.")
+			return 1		
+
+		with open(outName, 'wt') as resultFile:
+
+			with open(inName, 'rt') as inputFile:
+
+				inData = inputFile.read().splitlines()
+
+				inArr = 0
+				inTot = 1
+
+				while (inArr < len(inData)):
+					curArray = inData[inArr]
+
+					curArray = curArray.replace('[', '')
+					curArray = curArray.replace(']', '')
+
+					curArray = map(int, curArray.split(','))
+
+					curTotal = int(inData[inTot])
+
+					retArr, retCount = chGreedy(curArray, curTotal)
+
+					print("Coin count: " + str(retArr))
+					print("Minimal coins: " + str(retCount))
+
+					resultFile.write(str(retArr) + "\n")
+					resultFile.write(str(retCount) + "\n")
+
+					inArr = inArr + 2
+					inTot = inTot + 2
+
+	else:
+		print("ERROR: incorrect amount of arguments")
+
+
+main()
